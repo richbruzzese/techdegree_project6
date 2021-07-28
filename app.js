da5f95fee214bcdb6f851ab61396656778121618
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-07-28 09:04:51 -0400

    Error Handlers
    created .pug files for managing errors
    added error handling to app.js

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ app.get('/', (req, res) =>{
 app.listen(3000, () => {
     console.log('app running on port 3000')
 })
+
+app.use((req, res, next) => {
+  
+    res.status(404).render('404-error')
+  });
+  
+app.use((err, req, res, next) => {
+    res.locals.error = err
+    if (err.status === 404) {
+      res.status(404).render('404-error', {err})
+    }else{
+      err.message = err.message || `Something went wrong on the server`
+      res.status(err.status || 500).render('global-error', {err})
+    }
+  
+  });
