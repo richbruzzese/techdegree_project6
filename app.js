e43ac38a983da5c81316d65901cf5332960161f4
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-07-28 19:07:39 -0400

    changes
    - finished up code for additional page routes
    - removed comments on the page
    - updated images for project 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {quotes} = require('./data')
+const {projects} = require('./data')
 
 const app = express();
 
@@ -10,25 +10,40 @@ app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) =>{
-    res.render('index')
+    res.render('index', {title: 'Projects', projects})
 })
 
-app.listen(3000, () => {
-    console.log('app running on port 3000')
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+app.get('/project/:id', (req, res, next) =>{
+    const id = req.params.id
+    const project = projects[id]
+    if(project){
+        res.render('project', {project})   
+      }else{
+        next(err)
+      }
 })
 
 app.use((req, res, next) => {
-  
-    res.status(404).render('404-error')
+    const err = new Error("Not Found")
+        err.status = 404
+        next(err)
   });
   
 app.use((err, req, res, next) => {
-    res.locals.error = err
     if (err.status === 404) {
-      res.status(404).render('404-error', {err})
+        res.status(404).render('404-error', {err})
     }else{
-      err.message = err.message || `Something went wrong on the server`
-      res.status(err.status || 500).render('global-error', {err})
+        err.status = (err.status || 500)
+        err.message = (err.message || `Something went wrong on the server`)
+        res.render('global-error', {err})
     }
-  
+    console.error(err.status, err.message)
   });
+
+  app.listen(3000, () => {
+    console.log('app running on port 3000')
+})
